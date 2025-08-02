3b4d8f035ae8a8858c8ac412f235cea8b206c49b
ArmJava <armenjava@yandex.ru>
2025-08-02 14:16:55 +0000

    update: new function

---
+++ test.py
@@ -0,0 +1,32 @@
+# from handle import Recommendations
+
+# rec_store = Recommendations()
+
+# rec_store.load(
+#     "personal",
+#     'final_recommendations_feat.parquet',
+#     columns=["user_id", "item_id", "rank"],
+# )
+# rec_store.load(
+#     "default",
+#     'top_recs.parquet',
+#     columns=["item_id", "rank"],
+# )
+
+# res = rec_store.get(user_id=1049126, k=5)
+# print(res)
+import requests
+
+recommendations_url = "http://127.0.0.1:8000"
+
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+params = {"user_id": 1353637, 'k': 3}
+
+resp = requests.post(recommendations_url + "/recommendations", headers=headers, params=params)
+if resp.status_code == 200:
+    recs = resp.json()
+else:
+    recs = []
+    print(f"status code: {resp.status_code}")
+    
+print(recs)
